e246437a7f96151899cb6279cf3a095b8e8ab67a
calabi <alexandra_kozma@mail.ru>
2026-04-03 11:38:14 +0300

    upd kafka producer

--- src/messaging/kafka_producer.py
+++ src/messaging/kafka_producer.py
@@ -9,6 +9,11 @@ class KafkaProducerService:
         self.producer = KafkaProducer(
             bootstrap_servers=settings.kafka_bootstrap_servers,
             value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+
+            security_protocol=settings.kafka_security_protocol,
+            sasl_mechanism=settings.kafka_sasl_mechanism,
+            sasl_plain_username=settings.kafka_username,
+            sasl_plain_password=settings.kafka_password,
         )
 
         self.topic = settings.kafka_topic_predictions
